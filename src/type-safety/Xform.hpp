e683bbd0d1bad4036d9e308b428f78a1c04d871c
Mikołaj Radwan <mikoszrrr@gmail.com>
2019-02-15 17:32:12 +0100

    Working on xform appending

--- src/type-safety/Xform.hpp
+++ src/type-safety/Xform.hpp
@@ -3,6 +3,7 @@
 #include <type_traits>
 
 #include "Matrix.hpp"
+#include "CompressedPair.hpp"
 
 namespace type_safety {
 
@@ -11,36 +12,52 @@ struct EmptySpace {
 	};
 };
 
-//template <class FromSpaceT, class ToSpaceT>
-//class Xform : detail::FromToStorage<FromSpaceT, ToSpaceT> {
-//public:
-//
-//	using detail::FromToStorage<FromSpaceT, ToSpaceT>::fromSpace;
-//	using detail::FromToStorage<FromSpaceT, ToSpaceT>::toSpace;
-//
-//	Xform(
-//		typename FromSpaceT::ConstructorArgs fromSpaceArgs,
-//		typename ToSpaceT::ConstructorArgs toSpaceArgs
-//	) :
-//		detail::FromToStorage<FromSpaceT, ToSpaceT>(std::move(fromSpaceArgs), std::move(toSpaceArgs))
-//	{
-//	}
-//
-//	template <class OtherFromSpaceT, class OtherToSpaceT>
-//	auto then(const Xform<OtherFromSpaceT, OtherToSpaceT>& other) const {
-//		static_assert(compileTimeSpacesMatch<ToSpaceT, OtherFromSpaceT>());
-//		if (!runtimeSpacesMatch(toSpace(), other.fromSpace())) {
-//			assert(false);
-//		}
-//
-//		return true;
-//	}
-//
-//private:
-//
-//	Matrix matrix_;
-//
-//};
+template <class FromSpaceT, class ToSpaceT>
+constexpr bool spaceTypesMatch(FromSpaceT, ToSpaceT) {
+	return std::is_same_v<FromSpaceT, ToSpaceT>;
+}
+
+template <class FromSpaceT, class ToSpaceT>
+constexpr bool spacesMatch(FromSpaceT from_space, ToSpaceT to_space) {
+	static_assert(
+		spaceTypesMatch(from_space, to_space) || spaceTypesMatch(to_space, from_space),
+		"Space types don't match"
+	);
+	return true;
+}
+
+template <class FromSpaceT, class ToSpaceT>
+class Xform : CompressedPair<FromSpaceT, ToSpaceT> {
+public:
+
+	template <class... SpaceParams>
+	Xform(SpaceParams&&... spaceParams) :
+		CompressedPair<FromSpaceT, ToSpaceT>(std::forward<SpaceParams>(spaceParams)...)
+	{
+	}
+
+	decltype(auto) fromSpace() const {
+		return CompressedPair<FromSpaceT, ToSpaceT>::first();
+	}
+
+	decltype(auto) toSpace() const {
+		return CompressedPair<FromSpaceT, ToSpaceT>::second();
+	}
+
+	template <class OtherFromSpaceT, class OtherToSpaceT>
+	auto then(const Xform<OtherFromSpaceT, OtherToSpaceT>& other) const {
+		if (!spacesMatch(toSpace(), other.fromSpace())) {
+			assert(false);
+		}
+
+		return true;
+	}
+
+private:
+
+	Matrix matrix_;
+
+};
 
 namespace space {
 
--- test/type-safety/Xform.cpp
+++ test/type-safety/Xform.cpp
@@ -11,28 +11,73 @@ namespace /* anonymous */ {
 
 struct IntSpace {
 	int marker;
+
+	explicit IntSpace(int marker) :
+		marker(marker)
+	{
+	}
 };
 
-//TEST(XformTest, XformEmptySpaceAccess) {
-//	auto xform = Xform<space::World, space::Player>{};
-//	auto from = xform.fromSpace();
-//	auto to = xform.toSpace();
-//
-//	static_assert(std::is_same_v<decltype(from), space::World>);
-//	static_assert(std::is_same_v<decltype(to), space::Player>);
-//
-//	static_assert(sizeof(xform) == sizeof(Matrix));
-//}
-//
-//TEST(XformTest, XformFromIntToEmptySpaceAccess) {
-//	auto xform = Xform<IntSpace, space::Player>{};
-//	auto from = xform.fromSpace();
-//	auto to = xform.toSpace();
-//
-//	static_assert(std::is_same_v<decltype(from), IntSpace>);
-//	static_assert(std::is_same_v<decltype(to), space::Player>);
-//
-//	static_assert(sizeof(xform) == sizeof(Matrix));
-//}
+bool spacesMatch(IntSpace lhs, IntSpace rhs) {
+	return lhs.marker == rhs.marker;
+}
+
+struct WildcardSpace {
+};
+
+template <class SpaceT>
+constexpr bool spaceTypesMatch(WildcardSpace, SpaceT) {
+	return true;
+}
+
+ TEST(XformTest, XformEmptySpaceAccess) {
+	auto xform = Xform<space::World, space::Player>{};
+	auto from = xform.fromSpace();
+	auto to = xform.toSpace();
+
+	static_assert(std::is_same_v<decltype(from), space::World>);
+	static_assert(std::is_same_v<decltype(to), space::Player>);
+
+	static_assert(sizeof(xform) == sizeof(Matrix));
+}
+
+TEST(XformTest, XformFromIntToEmptySpaceAccess) {
+	auto xform = Xform<IntSpace, space::Player>{IntSpace{42}};
+	auto from = xform.fromSpace();
+	auto to = xform.toSpace();
+
+	static_assert(std::is_same_v<decltype(from), IntSpace>);
+	static_assert(std::is_same_v<decltype(to), space::Player>);
+
+	EXPECT_EQ(from.marker, 42);
+
+	static_assert(sizeof(xform) == sizeof(Matrix) + sizeof(int));
+}
+
+TEST(XformTest, CanAppendXformsWithCompileTimeMatchingSpaces) {
+	auto wtc = Xform<space::World, space::Camera>{};
+	auto ctp = Xform<space::Camera, space::Player>{};
+
+	wtc.then(ctp);
+	// Should not compile:
+	// ctp.then(wtc);
+}
+
+TEST(XformTest, CanOverrideCompileTimeSpaceMatching) {
+	auto wtc = Xform<space::World, space::Camera>{};
+	auto atp = Xform<WildcardSpace, space::Player>{};
+
+	wtc.then(atp);
+	atp.then(wtc);
+}
+
+TEST(XformTest, CanOverrideRuntimeSpaceMatching) {
+	auto wti3 = Xform<space::World, IntSpace>{3};
+	auto i3tp = Xform<IntSpace, space::Player>{3};
+	auto i2tp = Xform<IntSpace, space::Player>{2};
+
+	wti3.then(i3tp);
+	wti3.then(i2tp);
+}
 
 } // anonymous namespace
